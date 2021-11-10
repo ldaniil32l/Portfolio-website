70beccc3bb1e22411a6e06c5e8352b78e42a2cc6
ldaniil32l <popov.dan.2016@gmail.com>
2021-11-10 16:40:05 +0300

    add functional burger-menu and prohibiting scrolling when opening a modal window

--- js/index.js
+++ js/index.js
@@ -1,47 +1,105 @@
+const disabledScroll = () => {
 
-const presentOrderBtn = document.querySelector('.present__order-btn');
-const pageOverlayModal = document.querySelector('.page__overlay_modal');
-const modalClose = document.querySelector('.modal__close');
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
 
-pageOverlayModal.classList.add('page__overlay_modal_open');
-pageOverlayModal.classList.remove('page__overlay_modal_open');
+    document.body.scrollPosition = window.scrollY;
 
-const handlerModal = (openBtn, modal, openSelector, closeTrigger, sk = 'medium') => {
+    document.body.style.cssText = `
+    overflow: hidden;
+    position: fixed;
+    top: -${document.body.scrollPosition}px;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    padding-right: ${widthScroll}px;
+    `;
+}
 
-    let opacity = 0;
+const enabledScroll = () => {
+    document.body.style.cssText = 'position: relative;';
+    window.scroll({top: document.body.scrollPosition});
+}
 
-    const speed = {
-        slow: 10,
-        medium: 5,
-        fast: 1,
+{ // модальное окно
+    const presentOrderBtn = document.querySelector('.present__order-btn');
+    const pageOverlayModal = document.querySelector('.page__overlay_modal');
+    const modalClose = document.querySelector('.modal__close');
+
+    pageOverlayModal.classList.add('page__overlay_modal_open');
+    pageOverlayModal.classList.remove('page__overlay_modal_open');
+
+    const handlerModal = (openBtn, modal, openSelector, closeTrigger, sk = 'medium') => {
+
+        let opacity = 0;
+
+        const speed = {
+            slow: 0.02,
+            medium: 0.05,
+            fast: 0.1,
+        };
+
+        const openModal = () => {
+            disabledScroll();
+            modal.classList.add(openSelector);
+
+            const anim = () => {
+                opacity += speed[sk];
+                modal.style.opacity = opacity;
+                if (opacity < 1) requestAnimationFrame(anim);
+            };
+            requestAnimationFrame(anim);
+        };
+
+        const closeModal = () => {
+            enabledScroll();
+            const anim = () => {
+                opacity -= speed[sk];
+                modal.style.opacity = opacity;
+                if (opacity > 0) {
+                    requestAnimationFrame(anim);
+                } else {
+                    modal.classList.remove(openSelector);
+                }
+            };
+            requestAnimationFrame(anim);
+        };
+
+        openBtn.addEventListener('click', openModal);
+
+        closeTrigger.addEventListener('click', closeModal);
+
+        modal.addEventListener('click', (event) => {
+            if (event.target === modal) {
+                closeModal();
+            }
+        })
     };
 
-    openBtn.addEventListener('click', () => {
-        modal.classList.add(openSelector);
+    handlerModal(
+        presentOrderBtn,
+        pageOverlayModal,
+        'page__overlay_modal_open',
+        modalClose
+    );
+}
 
-        const timer = setInterval(() => {
-            opacity += 0.02;
-            modal.style.opacity = opacity;
-            if (opacity >= 1) clearInterval(timer);
-        }, speed[sk] ? speed[sk] : speed.default);
-    });
+{ // бургер-меню
 
-    closeTrigger.addEventListener('click', () => {
+    const headerContactsBurger = document.querySelector('.header__contacts-burger');
+    const headerContacts = document.querySelector('.header__contacts');
 
-        const timer = setInterval(() => {
-            opacity -= 0.02;
-            modal.style.opacity = opacity;
-            if (opacity <= 0) {
-                clearInterval(timer);
-                modal.classList.remove(openSelector);
+    const handlerBurger = (openBtn, menu, openSelector) => {
+        openBtn.addEventListener('click', () => {
+            if (menu.classList.contains(openSelector)) {
+                menu.style.height = '';
+                menu.classList.remove(openSelector);
+            } else {
+                menu.style.height = menu.scrollHeight + 'px';
+                menu.classList.add(openSelector);
             }
-        }, speed[sk] ? speed[sk] : speed.default);
-    });
-};
+        })
+    };
 
-handlerModal(
-    presentOrderBtn,
-    pageOverlayModal,
-    'page__overlay_modal_open',
-    modalClose
-);
+    handlerBurger(headerContactsBurger, headerContacts, 'header__contacts_open');
+
+}
